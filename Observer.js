374d24b0adcdadb8547231ec7c096826e192c744
tangquankun <1072317596@qq.com>
2017-10-10 14:18:06 +0800

    完成observer

--- Observer.js
+++ Observer.js
@@ -28,6 +28,20 @@ Observer.prototype = {
 		Object.defineProperty(data, key, {
 			enumerable: true, //可枚举
 			configurable: false,//不能够在define
+			get: function(){
+				if (Dep.target) {
+					dep.append()
+				}
+				return val
+			},
+			set: function(newVal){
+				if (val === newVal) {return}
+				val = newVal
+				//如果新的值是objec进行监听
+				childObj = observer(newVal)
+				//通知订阅者
+				dep.notify()
+			}
 		})
 	}
 }
@@ -42,6 +56,17 @@ Dep.prototype = {
 	//添加订阅者
 	addSub: function(sub){
 		this.subs.push(sub)
+	},
+	// 依赖
+	depend: function(){
+		Dep.target.addDep(this)
+	},
+	//移动订阅者
+	removeSubs: function(sub){
+		var index = this.subs.indexOf(sub) //获取订阅者的位置
+		if (index != -1) {
+			this.subs.splice(index,1) //移除订阅者
+		}
 	},
 	//通知订阅者
 	notify:function(){
@@ -58,65 +83,4 @@ function observer(value){
 	return new Observer(value)
 }
 
-
-
-
-
-
-
-
-/*
-var data = {
-	name: "may"
-}
-observer(data)
-
-function observer(data) {
-	if (!data || typeof data !== 'object') {
-		return
-	}
-	// 取出所有属性进行遍历,遍历出key值，
-	// Object.keys ==  for...in
-	Object.keys(data).forEach(function(key){
-		defineReactive(data,key,data[key])
-	})
-}
-
-
-function defineReactive(data,key,val){
-	var dep = new Dep()
-	observer(val)	//监听子属性
-	Object.defineProperty(data, key, {
-		enumerable: true, //可被枚举
-		configurable: false,//不可在defin
-		get: function(val){
-			console.log(Dep.target)
-			return val
-		},
-		set: function(newVal){
-			if (val === newVal) return
-			dep.notify() //通知所有订阅者
-			console.log('监听到值发生了变',val,'-->newVal')
-			val = newVal
-		}
-	})
-
-}
-
-//定义一个消息订阅器
-function Dep(){
-	this.subs = [] //订阅者
-}
-
-Dep.prototype = {
-	//添加订阅者
-	addSub: function(sub){
-		this.subs.push(sub)
-	},
-	//通知订阅者
-	notify:function(){
-		this.subs.forEach(function(sub){
-			sub.update() //让订阅者更新
-		})
-	}
-}*/
+Dep.target = null
